6e164eff27def3cef0efe6f7dec8757770a5bf6e
MarceloArraes <marcelo.arraes@gmail.com>
2022-08-09 11:46:55 +0000

    feat: using cluster to create workers

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 const express = require('express')
+const cluster = require('cluster');
+const os = require('os');
 
 const app = express()
 
-function delay(duration) {
-  const startTime = Date.now();
-  while (Date.now() - startTime < duration) {
-    //event loop blocked.
-    console.log('on loop');
-  }
-  return;
-}
-
 app.get('/', (req, res) => {
+  console.log('/', process.pid);
   res.send('Performance example');
 });
 
 app.get('/timer', (req, res) => {
-  delay(9000);
+  console.log('timer', process.pid);
   res.send('NEO, WAKE UP');
 });
+
+if (cluster.isMaster) {
+  console.log('Master process started');
+  console.log('os ', os.cpus(), os.cpus().length, os.hostname, os.EOL);
+  const cpus = os.cpus().length;
+  for (let i = 0; i < cpus; i++) {
+    cluster.Worker();
+  }
+
+}else console.log('Worker process started');
 
 app.listen(3000)
