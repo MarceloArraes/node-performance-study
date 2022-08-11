7f9faf8e639a2e02062ca439705e58c1491c38cd
MarceloArraes <marcelo.arraes@gmail.com>
2022-08-11 11:21:39 +0000

    feat: added pm2 to run the clusters for us

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 const express = require('express')
-const cluster = require('cluster');
-const os = require('os');
 
 const app = express()
 
 app.get('/', (req, res) => {
-  console.log('/', process.pid);
   res.send('Performance example');
 });
 
 app.get('/timer', (req, res) => {
-  console.log('timer', process.pid);
   res.send('NEO, WAKE UP');
 });
 
-if (cluster.isMaster) {
+/* if (cluster.isMaster) {
   console.log('Master process started');
   console.log('os ', os.cpus(), os.cpus().length, os.hostname, os.EOL);
   const cpus = os.cpus().length;
@@ -22,6 +18,6 @@ if (cluster.isMaster) {
     cluster.Worker();
   }
 
-}else console.log('Worker process started');
+}else console.log('Worker process started'); */
 
 app.listen(3000)
