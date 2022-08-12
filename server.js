0dc02573794874cac583e6db0a6fc5fd6c70c6dc
MarceloArraes <marcelo.arraes@gmail.com>
2022-08-12 11:43:28 +0000

    not working properly, but i will keep as register.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const { isMainThread, Worker, workerData } = require('worker_threads')
+
 
 const app = express()
 
@@ -10,6 +12,12 @@ app.get('/timer', (req, res) => {
   res.send('NEO, WAKE UP');
 });
 
+if (isMainThread) {
+  console.log('Main thread');
+  new Worker(__filename, { workerData: 'hello' })
+} else {
+  console.log("worker", workerData)
+}
 /* if (cluster.isMaster) {
   console.log('Master process started');
   console.log('os ', os.cpus(), os.cpus().length, os.hostname, os.EOL);
